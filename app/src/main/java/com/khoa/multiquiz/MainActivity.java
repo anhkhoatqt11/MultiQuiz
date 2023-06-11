7ec05b010b318820449a35684e5fe56cd130fcf2
Trương Anh Khoa <anhkhoatqt11@gmail.com>
2023-06-11 14:41:58 +0700

    Set up Bottom Navigation Bar

--- app/src/main/java/com/khoa/multiquiz/MainActivity.java
+++ app/src/main/java/com/khoa/multiquiz/MainActivity.java
@@ -1,15 +1,23 @@
 package com.khoa.multiquiz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.khoa.multiquiz.fragment.HomeFragment;
+import com.khoa.multiquiz.fragment.UserFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     FirebaseUser currentUser;
     Button logoutButton;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,20 +33,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         MAuth = FirebaseAuth.getInstance();
-        textView = findViewById(R.id.userCurrentLogin);
+//        textView = findViewById(R.id.userCurrentLogin);
         currentUser = MAuth.getCurrentUser();
-        logoutButton = findViewById(R.id.logoutButton);
+//        logoutButton = findViewById(R.id.logoutButton);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener(){
 
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment;
+                switch (item.getItemId()){
+                    case R.id.menu_mainpage_navigation:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case R.id.menu_userpage_navigation:
+                        selectedFragment = new UserFragment();
+                        break;
+                    default:
+                        return false;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.FrameContainer, selectedFragment).commit();
+
+                return true;
+            }
+        });
+
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.FrameContainer, new HomeFragment()).commit();
 
         if (currentUser == null){
             Intent intent = new Intent(getApplicationContext(), Login.class);
             startActivity(intent);
             finish();
         }else {
-            textView.setText(currentUser.getEmail());
+//            textView.setText(currentUser.getEmail());
         }
 
-        logoutButton.setOnClickListener(new View.OnClickListener() {
+/*        logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 MAuth.signOut();
@@ -45,6 +78,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        });
+        });*/
     }
 }
